ba773c7b74962811eeda4ab39d553bf7cdd37262
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-27 23:05:51 +0700

    Delete sampleRun.sh

--- sampleRun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/usr/bin/docker \
-	run -d -p 9091:9091 \
-	--name btServer \
-	-v /serverSidePath/Completed:/transmission/download \
-	-v /serverSidePath/Incomplete:/transmission/incomplete \
-	-v /serverSidePath/Downloads/Torrents:/transmission/watch \
-	-v /serverSidePath/.config:/transmission/config \
-	mnhan32/transmission:1.0
